8f1a5b2d5de75dc172f873ba9893ac49b70d4a9a
nao <iromnao60@gmail.com>
2026-07-23 00:20:48 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from "express"
+import jwt from "jsonwebtoken"
+const app = express();
+
+app.post("/api/v1/signin",(req,res)=>{
+
+})
+
+app.post("/api/v1/content",(req,res)=>{
+
+})
+
+app.get("/api/v1/content",(req,res)=>{
+
+})
+
+app.delete("/api/v1/content",(req,res)=>{
+
+})
+
+app.get("/api/v1/brain/:sharelink",(req,res)=>{
+
+})
